942cba501a6af46c54f13406a8e8b10cdd5804d3
Evan <eafavlaw@gmail.com>
2016-05-29 13:18:04 -0500

    basic pacstrap

---
+++ cac/pacstrap_list.sh
@@ -0,0 +1,5 @@
+pac_list=('bash' 'bzip2' 'coreutils' 'device-mapper' 'diffutils' 'e2fsprogs' 'file' 'filesystem' 'findutils' 'gawk' 'gettext' 'grep' 'gzip' 'inetutils' 'iproute2' 'iputils' 'less' 'licenses' 'logrotate' 'man-db' 'man-pages' 'pacman' 'pciutils' 'perl' 'procps-ng' 'psmisc' 'sed' 'shadow' 'sysfsutils' 'systemd-sysvcompat' 'tar' 'texinfo' 'usbutils' 'util-linux' 'which' 'sudo' 'nftables' 'vim' 'syslinux' 'openssh' 'tree' 'tmux' 'htop' 'lsof' 'lynx')
+	
+echo "Pacstrapping"
+pacstrap /mnt "${pac_list[@]}" || echo 'failed to pacstrap'
+done
